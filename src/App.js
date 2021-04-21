43faf3345dc337d5868928793a5e8ceb98a6a3b7
donika.p <i_donika.p@olcademy.in>
2021-04-22 00:18:29 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
+import Keypad from './components/Keypad'
+import Output from './components/Output';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    result: ''
+  }
+  buttonPressed = buttonName =>  {
+
+    if(buttonName === "=")
+    {
+      this.calculate()
+    }
+    else if(buttonName === "clr")
+    {
+      this.clear()
+    }
+    else if(buttonName === "ac")
+    {
+      this.AllClear()
+    }
+     else this.setState({
+      result: this.state.result + buttonName
+    });
+  };  
+  clear = () =>  {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    });
+  }
+  AllClear = () =>  {
+    this.setState({
+      result: " "
+    });
+  }
+  calculate = () =>  {
+    try{this.setState({
+      result: (eval(this.state.result) || " ") + " "
+    });
+  } catch (e) {
+      this.setState({
+        result: "error"
+      });
+    }
+  };
+  render() 
+  {
+    return (
+      <div className="App">
+        <div className="calc-body">
+          <Output results={this.state.result}/>
+          <Keypad buttonPressed={this.buttonPressed}/>      
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
